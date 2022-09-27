d25edf4dde4a81a0e9307ec7d74c9899a200b269
Adam de Leeuw <deleeuw@uk.ibm.com>
2022-09-27 14:07:35 +0100

    corrected module name

--- test/stages/stage3-outputs.tf
+++ test/stages/stage3-outputs.tf
@@ -3,12 +3,12 @@ resource local_file write_outputs {
   filename = "gitops-output.json"
 
   content = jsonencode({
-    name        = module.gitops_module.name
-    branch      = module.gitops_module.branch
-    namespace   = module.gitops_module.namespace
-    server_name = module.gitops_module.server_name
-    layer       = module.gitops_module.layer
-    layer_dir   = module.gitops_module.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_module.layer == "services" ? "2-services" : "3-applications")
-    type        = module.gitops_module.type
+    name        = module.gitops_watson-nlp.name
+    branch      = module.gitops_watson-nlp.branch
+    namespace   = module.gitops_watson-nlp.namespace
+    server_name = module.gitops_watson-nlp.server_name
+    layer       = module.gitops_watson-nlp.layer
+    layer_dir   = module.gitops_watson-nlp.layer == "infrastructure" ? "1-infrastructure" : (module.gitops_watson-nlp.layer == "services" ? "2-services" : "3-applications")
+    type        = module.gitops_watson-nlp.type
   })
 }
